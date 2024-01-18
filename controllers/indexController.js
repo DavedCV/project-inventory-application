5cba4bf67b69648e7c9db7f021dd3c6d853cbe36
DavedCV <97773641+DavedCV@users.noreply.github.com>
2024-01-18 11:27:15 -0500

    Change index home page

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -4,10 +4,5 @@ const Category = require("../models/Category");
 const asyncHandler = require("express-async-handler");
 
 exports.index = asyncHandler(async (req, res, next) => {
-  const allItems = await Item.find({}, "name category price")
-    .sort({ name: 1 })
-    .populate("category")
-    .exec();
-
-  res.render("index", { title: "All Items", items: allItems });
+  res.render("index");
 });
